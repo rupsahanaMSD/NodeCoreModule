acc642e834ce04a9f825653a17f3a26126288c48
rupsahanaMSD <binterashid94@gmail.com>
2024-01-25 00:10:30 +0600

    two file added

---
+++ school.js
@@ -0,0 +1,20 @@
+const EventEmitter = require ('events');
+const { setTimeout } = require('timers');
+class School extends EventEmitter{
+startPeriod(){
+    console.log('Class Started');
+    
+    //raise event when bell rings
+    //raise an event
+
+    setTimeout(()=>{
+        this.emit('bellRing', {
+            period: 'first',
+            text: 'period ended',
+        });
+    }, 2000);
+}
+
+}
+
+module.exports=School;
